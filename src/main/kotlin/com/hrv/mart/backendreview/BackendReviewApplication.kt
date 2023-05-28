8fbe0c1078e3dce67ccb4e6f82b19dd11e519d78
Harsh3305 <harshverma3305@gmail.com>
2023-05-28 18:06:57 +0530

    Initialise project

---
+++ src/main/kotlin/com/hrv/mart/backendreview/BackendReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.backendreview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendReviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendReviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/backendreview/BackendReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.backendreview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendReviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
