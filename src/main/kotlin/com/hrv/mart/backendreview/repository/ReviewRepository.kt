37c847be6dce2568953dce1de15b2669a33272a8
Harsh3305 <harshverma3305@gmail.com>
2023-05-29 16:20:51 +0530

    Added some funcitions in repository

--- src/main/kotlin/com/hrv/mart/backendreview/repository/ReviewRepository.kt
+++ src/main/kotlin/com/hrv/mart/backendreview/repository/ReviewRepository.kt
@@ -4,10 +4,15 @@ import com.hrv.mart.backendreview.model.Review
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
 interface ReviewRepository : ReactiveMongoRepository<Review, String> {
-    fun findByUserIdAndProductId(userId: String, productId: String): Flux<Review>
+    fun findByUserIdAndProductId(userId: String, productId: String): Mono<Review>
     fun findByProductId(productId: String): Flux<Review>
     fun findByUserId(userId: String): Flux<Review>
+    fun existsByUserIdAndProductId(userId: String, productId: String): Mono<Boolean>
+    fun countByUserId(userId: String): Mono<Long>
+    fun countByProductId(productId: String): Mono<Long>
+    fun deleteByUserIdAndProductId(userId: String, productId: String): Mono<Void>
 }
