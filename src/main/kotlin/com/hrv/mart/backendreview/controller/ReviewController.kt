f6f5d821ff587ef7a4e0a7d5890db76bd6e6f2fe
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-05-29 10:49:20 +0530

    Add service and repository layer (#9)
    * Added review repository
    
    * Added service and linked with controller

--- src/main/kotlin/com/hrv/mart/backendreview/controller/ReviewController.kt
+++ src/main/kotlin/com/hrv/mart/backendreview/controller/ReviewController.kt
@@ -1,18 +1,27 @@
 package com.hrv.mart.backendreview.controller
 
 import com.hrv.mart.backendreview.model.Review
+import com.hrv.mart.backendreview.service.ReviewService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import java.util.Optional
 
 @RestController
 @RequestMapping("/review")
-class ReviewController {
+class ReviewController (
+    @Autowired
+    private val reviewService: ReviewService
+)
+{
     @PostMapping
     fun createReview(@RequestBody review: Review) =
-        TODO("Link with service")
+        reviewService.createReview(review)
     @PutMapping
     fun updateReview(@RequestBody review: Review) =
-        TODO("Link with service")
+        reviewService.updateReview(review)
+    @DeleteMapping("/{reviewId}")
+    fun deleteReview(@PathVariable reviewId: String) =
+        reviewService.deleteReview(reviewId)
     @GetMapping
     fun getProductReview(
         @RequestParam productId: Optional<String>,
@@ -20,8 +29,25 @@ class ReviewController {
         @RequestParam size: Optional<Int>,
         @RequestParam page: Optional<Int>
     ) =
-        TODO("Link with service")
+        if (productId.isPresent) {
+            if (userId.isPresent) {
+                reviewService
+                    .getProductReviewPostedByUser(productId.get(), userId.get())
+            }
+            else {
+                reviewService
+                    .getProductReviews(productId.get())
+            }
+        }
+    else {
+        if (userId.isPresent) {
+            reviewService.getUserReview(userId.get())
+        }
+        else {
+            reviewService.getAllReview()
+        }
+    }
     @GetMapping("/{reviewID}")
     fun getReviewById(@PathVariable reviewID: String) =
-        TODO("Link with service")
+        reviewService.getReviewById(reviewID)
 }
