b1775848feae28603996582af704ebfc35544996
Harsh3305 <harshverma3305@gmail.com>
2023-08-07 18:44:09 +0530

    Update review list and did change name of constant parameters

--- src/test/kotlin/com/hrv/mart/backendreview/fixture/ReviewFixture.kt
+++ src/test/kotlin/com/hrv/mart/backendreview/fixture/ReviewFixture.kt
@@ -4,58 +4,66 @@ import com.hrv.mart.backendreview.model.Review
 import com.hrv.mart.userlibrary.model.User
 
 object ReviewFixture {
-    const val productId1 = "productId1"
-    const val productId2 = "productId2"
+    private const val PRODUCT_ID_1 = "productId1"
+    private const val PRODUCT_ID_2 = "productId2"
 
-    const val userId1 = "userId1"
-    const val userId2 = "userId2"
+    private const val USER_ID_1 = "userId1"
+    private const val USER_ID_2 = "userId2"
 
-    const val description = "Test Description"
-    val images = listOf(
+    private const val DESCRIPTION = "Test Description"
+    private val images = listOf(
         "https://image.test.com/1",
         "https://image.test.com/2",
         "https://image.test.com/3"
     )
-    const val title = "Test"
+    private const val TITLE = "Test"
 
     val allReviews = listOf(
         Review(
-            productId = productId1,
-            userId = userId1,
+            productId = PRODUCT_ID_1,
+            userId = USER_ID_1,
             images = images,
-            description = description,
-            title = title
+            description = DESCRIPTION,
+            title = TITLE+"1",
+            dateTimeOfReview = "07/08/2023 06:26:12"
         ),
         Review(
-            productId = productId1,
-            userId = userId2,
+            productId = PRODUCT_ID_1,
+            userId = USER_ID_2,
             images = images,
-            description = description,
-            title = title
+            description = DESCRIPTION,
+            title = TITLE+"2",
+            dateTimeOfReview = "07/08/2023 06:27:20"
         ),
         Review(
-            productId = productId2,
-            userId = userId1,
+            productId = PRODUCT_ID_2,
+            userId = USER_ID_1,
             images = images,
-            description = description,
-            title = title
+            description = DESCRIPTION,
+            title = TITLE+"4",
+            dateTimeOfReview = "07/08/2023 06:28:35"
         ),
         Review(
-            productId = productId2,
-            userId = userId2,
+            productId = PRODUCT_ID_2,
+            userId = USER_ID_2,
             images = images,
-            description = description,
-            title = title
+            description = DESCRIPTION,
+            title = TITLE+"4",
+            dateTimeOfReview = "07/08/2023 06:29:05"
         )
     )
+        .sortedBy {
+            it.dateTimeOfReview
+        }
+        .reversed()
     val allUsers = listOf(
         User(
             name = "Test User",
-            emailId = userId1
+            emailId = USER_ID_1
         ),
         User(
             name = "Test User",
-            emailId = userId2
+            emailId = USER_ID_2
         )
     )
 }
